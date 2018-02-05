aa128b9a7c4a9ed550d2071e28505cde744ce9ef
MrFlyingbananas <mrflyingbananas@yahoo.com>
2018-02-05 02:42:30 -0600

    Improved Glass
    Fixed glass drawing
    Added glass rotation

--- js/glass.js
+++ js/glass.js
@@ -1,16 +1,47 @@
 class Glass {
-  constructor(v1, v2, v3){
-    this.v1 = v1;
-    this.v2 = v2;
-    this.v3 = v3;
-  }
-  render(){
-    strokeWeight(10);
-    stroke(random(255), random(255), random(255));
-    line(this.v1.x, this.v1.y, this.v2.x, this.v2.y);
-    line(this.v2.x, this.v2.y, this.v3.x, this.v3.y);
-    line(this.v3.x, this.v3.y, this.v1.x, this.v1.y);
+  constructor(v1, v2, v3) {
 
+    this.center = createVector((v1.x + v2.x + v3.x) / 3, (v1.y + v2.y + v3.y) / 3);
+
+
+    this.v1 = p5.Vector.sub(v1,this.center)
+    this.v2 = p5.Vector.sub(v2,this.center)
+    this.v3 = p5.Vector.sub(v3,this.center)
+    // this.v2.sub(this.center)
+    // this.v3.sub(this.center)
+    console.log(this.v1,this.v2,this.v3,this.center)
+    // this.v1 = createVector(this.v1.x-this.center.x, this.v1.y-this.center.y)
+
+    this.rotMult = random(-2, 2);
+    this.rot = 0
+  }
+  incRot(amount) {
+    this.rot += amount;
+  }
+  render() {
+    strokeWeight(10);
+    stroke(214, 234, 248);
+    fill(52, 152, 219, 20);
+    // line(this.v1.x, this.v1.y, this.v2.x, this.v2.y);
+    // line(this.v2.x, this.v2.y, this.v3.x, this.v3.y);
+    // line(this.v3.x, this.v3.y, this.v1.x, this.v1.y);
+    push();
+
+    translate(this.center.x,this.center.y)
+    if (this.rot != 0) {
+      rotate(this.rot * this.rotMult);
+    }
+    triangle(
+      this.v1.x, this.v1.y,
+      this.v2.x, this.v2.y,
+      this.v3.x, this.v3.y);
+
+    stroke(255, 0, 0);
     point(this.v1.x, this.v1.y);
+    stroke(0, 255, 0);
+    point(this.v2.x, this.v2.y);
+    stroke(0, 0, 255);
+    point(this.v3.x, this.v3.y);
+    pop();
   }
 }
--- js/glassPane.js
+++ js/glassPane.js
@@ -1,39 +1,55 @@
-const sections = 3;
-let glass = []
-let pick = 0;
-function setup(){
+const sections = 2;
+let glass = [];
+let rot = 0;
+
+function setup() {
   pixelDensity(1);
   let canvas = createCanvas(windowWidth, windowHeight);
-  let incx = windowWidth/sections;
-  let incy = windowHeight/sections;
-  let points = []
+  let incx = windowWidth / sections;
+  let incy = windowHeight / sections;
+  let points = [];
+  rectMode(CENTER)
   canvas.parent('FlowField-holder');
   let range = 100;
-  for(let i = 0; i < sections; i++){
-    for(let j = 0; j < sections; j++){
-      xr = floor(random(range)-range);
-      yr = floor(random(range)-range);
-      xr = 0; yr = 0;
-      points[i * sections + j] = createVector(incx/2 + i*incx + xr,incy/2 + j*incy + yr);
+  for (let i = 0; i < sections + 1; i++) {
+    for (let j = 0; j < sections + 1; j++) {
+      let xr = 0;
+      let yr = 0;
+
+      if (j != 0 && j + 1 != sections + 1) {
+        xr = floor(random(range) - range);
+      }
+      if (i != 0 && i + 1 != sections + 1) {
+        yr = floor(random(range) - range);
+      }
+      xr=0;yr=0
+      points[i * (sections + 1) + j] = createVector(j * incx + xr, i * incy + yr);
     }
   }
-  let gsections = sections - 1;
-  for(let i = 0; i < gsections; i++){
-    for(let j = 0; j < gsections; j++){
-      glass[i * gsections + j] = new Glass(points[i * sections + j],points[(i) * sections + j + 1],points[(i+1) * sections + j]);
-      glass[gsections * gsections + j + i * gsections] = new Glass(points[(i+1) * sections + j + 1],points[(i+1) * sections + j],points[(i) * sections + j + 1]);
-
-      }
+  frameRate(30)
+  let gsections = sections;
+  for (let y = 0; y < gsections; y++) {
+    for (let x = 0; x < gsections; x++) {
+      glass[y * gsections + x] = new Glass(points[y * (sections + 1) + x], points[y * (sections + 1) + x + 1], points[(y + 1) * (sections + 1) + x]);
+      glass[gsections * gsections + y * gsections + x] = new Glass(points[y * (sections + 1) + x + 1], points[(y + 1) * (sections + 1) + x + 1], points[(y + 1) * (sections + 1) + x]);
+    }
   }
-  console.log(glass.length)
-  frameRate(1);
+  // glass[i * gsections + j] = new Glass(points[i * sections + (j+i)],points[i+1][j],points[i][j+1]);
+  //   // glass[gsections * gsections + j + i * gsections] = new Glass(points[(i+1) * sections + j + 1],points[(i+1) * sections + j],points[(i) * sections + j + 1]);
+  //   console.log("vert1:",i, j)
+  //   console.log("vert2:",i+1,j)
+  //   console.log("vert3:",i,j+1)
+
+  frameRate(30);
 
 }
 
-function draw(){
+function draw() {
+  // rot+=.001;
   background(220);
   strokeWeight(10);
-  for(g of glass){
+  for (let g of glass) {
+    g.incRot(.01)
     g.render();
   }
 }
