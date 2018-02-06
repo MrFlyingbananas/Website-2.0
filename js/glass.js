e76e99d0f3d1591bc22d253af3d77e42f9f8843c
MrFlyingbananas <mrflyingbananas@yahoo.com>
2018-02-05 23:58:57 -0600

    Updated glass rendering
    Now uses WebGL and lights

--- js/glass.js
+++ js/glass.js
@@ -1,47 +1,78 @@
 class Glass {
-  constructor(v1, v2, v3) {
+	constructor(v1, v2, v3) {
 
-    this.center = createVector((v1.x + v2.x + v3.x) / 3, (v1.y + v2.y + v3.y) / 3);
+		this.center = createVector((v1.x + v2.x + v3.x) / 3, (v1.y + v2.y + v3.y) / 3);
+		// this.v1 = p5.Vector.sub(v1, this.center);
+		// this.v2 = p5.Vector.sub(v2, this.center);
+		// this.v3 = p5.Vector.sub(v3, this.center);
+		// console.log(this.v1, this.v2, this.v3);
+		// console.log(this.v1.x == v1.sub(this.center).x, this.v2.y == v2.sub(this.center).y, this.v3 == v3.sub(this.center));
+		this.v1 = v1;
+		this.v2 = v2;
+		this.v3 = v3;
+		// this.v1.x -= this.center.x;
+		// this.v1.y -= this.center.y;
+		// this.v2.x -= this.center.x;
+		// this.v2.y -= this.center.y;
+		// this.v3.x -= this.center.x;
+		// this.v3.y -= this.center.y;
+		this.rotMult = random(-2, 2);
+		this.rot = 0;
+		this.vel = createVector(0, 0, 0);
+		this.pos = createVector(0, 0, 0);
+	}
+	addRot(amount) {
+		this.rot += amount;
+	}
+	render() {
+		// strokeWeight(10);
+		// stroke(214, 234, 248);
+		// fill(52, 152, 219, 20);
+		// line(this.v1.x, this.v1.y, this.v2.x, this.v2.y);
+		// line(this.v2.x, this.v2.y, this.v3.x, this.v3.y);
+		// line(this.v3.x, this.v3.y, this.v1.x, this.v1.y);
+		push();
+		this.pos.add(this.vel);
+		translate(this.center.x + this.pos.x, this.center.y + this.pos.x);
+		if (this.rot != 0) {
+			rotate(this.rot * this.rotMult);
+		}
 
-
-    this.v1 = p5.Vector.sub(v1,this.center)
-    this.v2 = p5.Vector.sub(v2,this.center)
-    this.v3 = p5.Vector.sub(v3,this.center)
-    // this.v2.sub(this.center)
-    // this.v3.sub(this.center)
-    console.log(this.v1,this.v2,this.v3,this.center)
-    // this.v1 = createVector(this.v1.x-this.center.x, this.v1.y-this.center.y)
-
-    this.rotMult = random(-2, 2);
-    this.rot = 0
-  }
-  incRot(amount) {
-    this.rot += amount;
-  }
-  render() {
-    strokeWeight(10);
-    stroke(214, 234, 248);
-    fill(52, 152, 219, 20);
-    // line(this.v1.x, this.v1.y, this.v2.x, this.v2.y);
-    // line(this.v2.x, this.v2.y, this.v3.x, this.v3.y);
-    // line(this.v3.x, this.v3.y, this.v1.x, this.v1.y);
-    push();
-
-    translate(this.center.x,this.center.y)
-    if (this.rot != 0) {
-      rotate(this.rot * this.rotMult);
-    }
-    triangle(
-      this.v1.x, this.v1.y,
-      this.v2.x, this.v2.y,
-      this.v3.x, this.v3.y);
-
-    stroke(255, 0, 0);
-    point(this.v1.x, this.v1.y);
-    stroke(0, 255, 0);
-    point(this.v2.x, this.v2.y);
-    stroke(0, 0, 255);
-    point(this.v3.x, this.v3.y);
-    pop();
-  }
+		beginShape();
+		// vertex(this.v1.x - this.center.x + this.pos.x, this.v1.y - this.center.y - this.pos.y, this.v1.z - this.pos.z);
+		vertex(this.v1.x - this.center.x, this.v1.y - this.center.y, this.v1.z);
+		// vertex(this.v2.x - this.center.x + this.pos.x, this.v2.y - this.center.y - this.pos.y, this.v2.z - this.pos.z);
+		vertex(this.v2.x - this.center.x, this.v2.y - this.center.y, this.v2.z);
+		// vertex(this.v3.x - this.center.x + this.pos.x, this.v3.y - this.center.y - this.pos.y, this.v3.z - this.pos.z);
+		vertex(this.v3.x - this.center.x, this.v3.y - this.center.y, this.v3.z);
+		endShape();
+		//
+		// stroke(255, 0, 0);
+		// point(this.v1.x, this.v1.y);
+		// stroke(0, 255, 0);
+		// point(this.v2.x, this.v2.y);
+		// stroke(0, 0, 255);
+		// point(this.v3.x, this.v3.y);
+		pop();
+	}
+	explode() {
+		var flip1 = random(0, 1);
+		if (flip1 > .5) {
+			flip1 = 1
+		} else {
+			flip1 = -1
+		}
+		var flip2 = random(0, 1);
+		if (flip2 > .5) {
+			flip2 = 1
+		} else {
+			flip2 = -1
+		}
+		this.vel.x = flip1 * random(10, 50);
+		this.vel.y = flip2 * random(10, 50);
+		this.vel.z = random(5, 50);
+		this.v1 = this.v1.copy();
+		this.v2 = this.v2.copy();
+		this.v3 = this.v3.copy();
+	}
 }
--- js/glassPane.js
+++ js/glassPane.js
@@ -1,55 +1,181 @@
-const sections = 2;
+const sections = 6;
 let glass = [];
-let rot = 0;
+
+let time = 0;
+let points = [];
+let inc = .5;
+let explode = false;
+var vs = [
+
+	// Precision
+	'precision mediump float;',
+
+	// Lights
+	'#define LIGHTS ' + lights,
+
+	// Attributes
+	'attribute float aSide;',
+	'attribute vec3 aPosition;',
+	'attribute vec3 aCentroid;',
+	'attribute vec3 aNormal;',
+	'attribute vec4 aAmbient;',
+	'attribute vec4 aDiffuse;',
+
+	// Uniforms
+	'uniform vec3 uResolution;',
+	'uniform vec3 uLightPosition[LIGHTS];',
+	'uniform vec4 uLightAmbient[LIGHTS];',
+	'uniform vec4 uLightDiffuse[LIGHTS];',
+
+	// Varyings
+	'varying vec4 vColor;',
+
+	// Main
+	'void main() {',
+
+	// Create color
+	'vColor = vec4(0.0);',
+
+	// Calculate the vertex position
+	'vec3 position = aPosition / uResolution * 2.0;',
+
+	// Iterate through lights
+	'for (int i = 0; i < LIGHTS; i++) {',
+	'vec3 lightPosition = uLightPosition[i];',
+	'vec4 lightAmbient = uLightAmbient[i];',
+	'vec4 lightDiffuse = uLightDiffuse[i];',
+
+	// Calculate illuminance
+	'vec3 ray = normalize(lightPosition - aCentroid);',
+	'float illuminance = dot(aNormal, ray);',
+	'if (aSide == 0.0) {',
+	'illuminance = max(illuminance, 0.0);',
+	'} else if (aSide == 1.0) {',
+	'illuminance = abs(min(illuminance, 0.0));',
+	'} else if (aSide == 2.0) {',
+	'illuminance = max(abs(illuminance), 0.0);',
+	'}',
+
+	// Calculate ambient light
+	'vColor += aAmbient * lightAmbient;',
+
+	// Calculate diffuse light
+	'vColor += aDiffuse * lightDiffuse * illuminance;',
+	'}',
+
+	// Clamp color
+	'vColor = clamp(vColor, 0.0, 1.0);',
+
+	// Set gl_Position
+	'gl_Position = vec4(position, 1.0);',
+
+	'}'
+
+	// Return the shader
+].join('\n');
+var fs = [
+
+	// Precision
+	'precision mediump float;',
+
+	// Varyings
+	'varying vec4 vColor;',
+
+	// Main
+	'void main() {',
+
+	// Set gl_FragColor
+	'gl_FragColor = vColor;',
+
+	'}'
+
+	// Return the shader
+].join('\n');
+var shad;
 
 function setup() {
-  pixelDensity(1);
-  let canvas = createCanvas(windowWidth, windowHeight);
-  let incx = windowWidth / sections;
-  let incy = windowHeight / sections;
-  let points = [];
-  rectMode(CENTER)
-  canvas.parent('FlowField-holder');
-  let range = 100;
-  for (let i = 0; i < sections + 1; i++) {
-    for (let j = 0; j < sections + 1; j++) {
-      let xr = 0;
-      let yr = 0;
+	pixelDensity(1);
+	let canvas = createCanvas(windowWidth, windowHeight, WEBGL);
+	shad = createShader(vs, fs);
+	let incx = windowWidth / sections;
+	let incy = windowHeight / sections;
+	canvas.parent('FlowField-holder');
+	let range = 80;
+	let zrange = 100;
+	for (let i = 0; i < sections + 1; i++) {
+		for (let j = 0; j < sections + 1; j++) {
+			let xr = 0;
+			let yr = 0;
+			let zr = 0;
+			if (j != 0 && j + 1 != sections + 1) {
+				xr = floor(random(range) - range);
+			}
+			if (i != 0 && i + 1 != sections + 1) {
+				yr = floor(random(range) - range);
+			}
+			if (xr != 0 && yr != 0) {
+				zr = random(-zrange, zrange);
+			}
+			points[i * (sections + 1) + j] = createVector(j * incx + xr, i * incy + yr, zr);
+		}
+	}
+	frameRate(30);
+	let gsections = sections;
+	for (let y = 0; y < gsections; y++) {
+		for (let x = 0; x < gsections; x++) {
+			glass[y * gsections + x] = new Glass(points[y * (sections + 1) + x], points[y * (sections + 1) + x + 1], points[(y + 1) * (sections + 1) + x]);
+			glass[gsections * gsections + y * gsections + x] = new Glass(points[y * (sections + 1) + x + 1], points[(y + 1) * (sections + 1) + x + 1], points[(y + 1) * (sections + 1) + x]);
+		}
+	}
 
-      if (j != 0 && j + 1 != sections + 1) {
-        xr = floor(random(range) - range);
-      }
-      if (i != 0 && i + 1 != sections + 1) {
-        yr = floor(random(range) - range);
-      }
-      xr=0;yr=0
-      points[i * (sections + 1) + j] = createVector(j * incx + xr, i * incy + yr);
-    }
-  }
-  frameRate(30)
-  let gsections = sections;
-  for (let y = 0; y < gsections; y++) {
-    for (let x = 0; x < gsections; x++) {
-      glass[y * gsections + x] = new Glass(points[y * (sections + 1) + x], points[y * (sections + 1) + x + 1], points[(y + 1) * (sections + 1) + x]);
-      glass[gsections * gsections + y * gsections + x] = new Glass(points[y * (sections + 1) + x + 1], points[(y + 1) * (sections + 1) + x + 1], points[(y + 1) * (sections + 1) + x]);
-    }
-  }
-  // glass[i * gsections + j] = new Glass(points[i * sections + (j+i)],points[i+1][j],points[i][j+1]);
-  //   // glass[gsections * gsections + j + i * gsections] = new Glass(points[(i+1) * sections + j + 1],points[(i+1) * sections + j],points[(i) * sections + j + 1]);
-  //   console.log("vert1:",i, j)
-  //   console.log("vert2:",i+1,j)
-  //   console.log("vert3:",i,j+1)
-
-  frameRate(30);
+	frameRate(30);
 
 }
 
 function draw() {
-  // rot+=.001;
-  background(220);
-  strokeWeight(10);
-  for (let g of glass) {
-    g.incRot(.01)
-    g.render();
-  }
+	time += .02;
+	let yoff = 0;
+	for (let i = 0; i < sections + 1; i++) {
+
+		let xoff = 0;
+		for (let j = 0; j < sections + 1; j++) {
+			if (i == 0 || j == 0 || i + 1 == sections + 1 || j + 1 == sections + 1) {
+				continue;
+			}
+			xoff += inc;
+			points[i * (sections + 1) + j].z = map(noise(xoff, yoff, time), 0, 1, -300, 300);
+		}
+		yoff += inc;
+	}
+
+	translate(-windowWidth / 2, -windowHeight / 2);
+	background(220);
+	// shader(mandel);
+	ambientLight(66, 0, 49, 20);
+	directionalLight(136, 0, 102, 0, -.25, -1);
+	directionalLight(136, 50, 100, 0, .25, -1);
+	directionalLight(136, 50, 100, .25, 0, -1);
+	directionalLight(136, 50, 100, -.25, 0, -1);
+	ambientMaterial(200);
+	push();
+	translate(windowWidth / 2, windowHeight / 2, -150);
+	// pointLight(255, 50, 100, windowWidth / 2, windowHeight / 2, 100);
+	// pointLight(255, 50, 100, windowWidth / 2, windowHeight / 2, 100);
+	pop();
+
+	// pointLight(255, 50, 100, windowWidth / 2, windowHeight / 2, 100);
+	// pointLight(255, 50, 100, windowWidth / 2, windowHeight / 2, 100);
+	console.log(time)
+	if (time > 2 && !explode) {
+		explode = true;
+		for (let g of glass) {
+			g.explode();
+		}
+	}
+	for (let g of glass) {
+		if (explode) {
+			g.addRot(.1);
+		}
+		g.render();
+	}
 }
